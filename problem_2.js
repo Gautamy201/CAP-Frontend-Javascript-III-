7164be99af29ef4185e8b16a1ea3e7236935105b
Gautamy201 <148048329+Gautamy201@users.noreply.github.com>
2023-11-27 19:08:12 +0530

    Update problem_2.js

--- problem_2.js
+++ problem_2.js
@@ -6,9 +6,16 @@ function findAbsoluteDifference(num1, num2) {
   return absoluteDifference;
 }
 
-// Example usage:
-var number1 = 8;
-var number2 = 3;
+// problem_1:
+var number1 = 12;
+var number2 = 4;
 
 var result = findAbsoluteDifference(number1, number2);
+console.log("The absolute difference is: " + result);
+
+// problem_2:
+var numbers1 = 4;
+var numbers2 = 18;
+
+var result = findAbsoluteDifference(numbers1, numbers2);
 console.log("The absolute difference is: " + result);
